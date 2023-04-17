b2833d299a66c999910001696a93309171ab99ab
hlee443 <hlee443@my.bcit.ca>
2023-04-17 13:42:10 -0700

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,6 +16,14 @@ export default function Projects() {
         appDescription="myCibo is a comprehensive food management mobile app designed to enhance users culinary experiences by efficiently tracking food inventory, devising customized recipes based on available ingredients, and supporting grocery receipt scanning for replenishing supplies."
       />
       <Project
+        img={sidekick}
+        siteLink="https://sidekick-front-end.vercel.app/"
+        githubLink="https://github.com/BinaryBusters/binary_busters_frontend.git"
+        appHeader="React, Tailwind, Flask(Python), OpenAI API, Render, Vercel"
+        appTitle="Sidekick"
+        appDescription="Sidekick - the all-in-one platform designed to help you stay ahead of the curve and excel in your career. Whether you're a recent graduate trying to break into the tech industry or a seasoned professional looking to upgrade your skills, Sidekick provides technical training, real-world practice scenarios, and targeted feedback to help you achieve your professional goals."
+      />
+      <Project
         img={pathfinder}
         siteLink="https://pathfinder-athena.vercel.app/"
         githubLink="https://github.com/hlee443/PathFinder-Athena.git"
@@ -30,14 +38,6 @@ export default function Projects() {
         appHeader="HTML/CSS, React, Javascript, Node.js, Express,Railway, Firebase"
         appTitle="WordGotchu"
         appDescription="WordGotchu is a Pixel styled multiplayer web platform game based on Wordle and original characters known as Gotchus that battle against each other in an arena."
-      />
-      <Project
-        img={sidekick}
-        siteLink="https://sidekick-front-end.vercel.app/"
-        githubLink="https://github.com/BinaryBusters/binary_busters_frontend.git"
-        appHeader="React, Tailwind, Flask(Python), OpenAI API, Render, Vercel"
-        appTitle="Sidekick"
-        appDescription="Sidekick - the all-in-one platform designed to help you stay ahead of the curve and excel in your career. Whether you're a recent graduate trying to break into the tech industry or a seasoned professional looking to upgrade your skills, Sidekick provides technical training, real-world practice scenarios, and targeted feedback to help you achieve your professional goals."
       />
     </>
   );
